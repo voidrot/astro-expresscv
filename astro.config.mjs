1f3f3f60d2d7939e975cf1d1299b6b21181c0448
Buck Brady <bbrady145@gmail.com>
2024-11-09 05:20:10 -0700

    add navbar, footer, and landing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,26 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import sitemap from "@astrojs/sitemap";
+
+import robotsTxt from "astro-robots-txt";
+
+import mdx from "@astrojs/mdx";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: process.env.ASTRO_SITE || "http://localhost:4321",
+  integrations: [sitemap(), mdx(), tailwind(), robotsTxt({
+    sitemap: true,
+    host: process.env.ASTRO_SITE || "http://localhost:4321",
+    policy: [
+      {
+        userAgent: "*",
+        allow: "/",
+        crawlDelay: 10
+      }
+    ]
+  })]
+});
